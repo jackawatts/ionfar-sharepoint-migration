c69c8500615a109076c8cdaa723bbb774979eedf
Sly Gryphon <sgryphon@computer.org>
2015-05-12 16:19:59 +0200

    Alternative existing context manager

---
+++ src/IonFar.SharePoint.Migration/Providers/ExistingContextManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace IonFar.SharePoint.Migration.Providers
+{
+    /// <summary>
+    /// A context manager that uses an existing ClientContext.
+    /// </summary>
+    public class ExistingContextManager : IContextManager
+    {
+        ClientContext _context;
+        IUpgradeLog _log;
+
+        public ExistingContextManager(ClientContext existingContext)
+        {
+            _context = existingContext;
+        }
+
+        /// <summary>
+        /// Gets the current context from the context manager
+        /// </summary>
+        public ClientContext CurrentContext
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
+        /// <summary>
+        /// Tells the context manager when it is starting, and completing, a migration context
+        /// </summary>
+        /// <param name="log">Log to use</param>
+        public IDisposable ContextScope(IUpgradeLog log)
+        {
+            _log = log;
+            return new NullDisposable();
+        }
+
+        class NullDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
--- src/TestApplication/Program.cs
+++ src/TestApplication/Program.cs
@@ -13,34 +13,34 @@ namespace TestApplication
     {
         private static void Main(string[] args)
         {
-            var config = new MigratorConfiguration() {};
-            //_logger = new ConsoleUpgradeLog();
-            //config.Log = new TraceUpgradeLog();
-
             if (args.Length != 3)
             {
                 Console.WriteLine("Format is: TestApplication.exe username password sitecollectionurl");
                 return;
             }
-
             string username = args[0];
             string password = args[1];
             string webUrl = args[2];
-
             SecureString securePassword = GetSecureStringFromString(password);
             ICredentials credentials = new SharePointOnlineCredentials(username, securePassword);
 
-            config.ContextManager = new BasicContextManager(webUrl, credentials);
+            var config = new MigratorConfiguration();
+            //config.Log = new ConsoleUpgradeLog();
+            //config.Log = new TraceUpgradeLog();
 
             config.MigrationProviders.Add(new AssemblyMigrationProvider(Assembly.GetAssembly(typeof(ShowTitle))));
             //config.Journal = new NullJournal();
 
+            config.ContextManager = new BasicContextManager(webUrl, credentials);
             var migrator = new Migrator(config);
-
             migrator.Migrate();
 
             //using (var clientContext = new ClientContext(webUrl))
             //{
+            //    clientContext.Credentials = credentials;
+            //    config.ContextManager = new ExistingContextManager(clientContext);
+            //    var migrator = new Migrator(config);
+            //    migrator.Migrate();
             //}
 
             Console.WriteLine("Done");
