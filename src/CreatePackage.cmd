668215dc4340edfefe2405fc9834e76b1e26ad73
Sly Gryphon <sgryphon@computer.org>
2015-09-01 12:57:44 +0000

    Combine into only one nuget package

--- src/CreatePackage.cmd
+++ src/CreatePackage.cmd
@@ -1,4 +1,3 @@
 REM Creates a NuGet package
 if not exist Output mkdir Output
 .nuget\nuget.exe pack IonFar.SharePoint.Migration\IonFar.SharePoint.Migration.csproj -Build -OutputDirectory Output
-.nuget\nuget.exe pack IonFar.SharePoint.Synchronization\IonFar.SharePoint.Synchronization.csproj -Build -OutputDirectory Output
