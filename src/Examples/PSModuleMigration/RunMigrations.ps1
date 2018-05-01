d4c7e1d8d267a1c7e19ea81c89b964aed8c96b24
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-05-01 14:07:38 +1000

    Complete Invoke-ScriptMigration cmdlet with example

---
+++ src/Examples/PSModuleMigration/RunMigrations.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+	Runs script migrations using IonFar PowerShell module
+
+.EXAMPLE
+	powershell .\RunMigrations.ps1 -SiteUrl https://tenant.sharepoint.com/sites/site-name -UserName XXX -Password XXX
+#>
+param(
+    [Parameter(Mandatory=$True)]
+    [string] $SiteUrl,
+
+    [Parameter(Mandatory=$True)]
+    [string] $UserName,
+
+    [Parameter(Mandatory=$True)]
+    [string] $Password
+)
+
+Import-Module "$($PSScriptRoot)\..\..\IonFar.SharePoint.PowerShell\bin\Debug\IonFar.SharePoint.PowerShell.dll"
+
+Invoke-ScriptMigration -SiteUrl $SiteUrl -UserName $UserName -Password $Password -ScriptDirectory "$($PSScriptRoot)\Migrations"
---
+++ src/IonFar.SharePoint.PowerShell/InvokeScriptMigrationCmdlet.cs
@@ -0,0 +1,53 @@
+﻿using IonFar.SharePoint.Migration;
+using IonFar.SharePoint.Migration.Journals;
+using IonFar.SharePoint.Migration.Logs;
+using IonFar.SharePoint.Migration.Providers.Script;
+using IonFar.SharePoint.Migration.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IonFar.SharePoint.PowerShell
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "ScriptMigration")]
+    public class InvokeScriptMigrationCmdlet: Cmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public string SiteUrl { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string UserName { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Password { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string ScriptDirectory { get; set; }
+
+        [Parameter]
+        public SwitchParameter Force { get; set; }
+
+        [Parameter]
+        public string WorkingDirectory { get; set; }
+
+        [Parameter]
+        public string JournalPrefix { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var config = new MigratorConfiguration
+            {
+                Log = new ConsoleUpgradeLog(true),
+                Journal = this.Force ? (IJournal)new NullJournal() : (IJournal)new WebPropertyBagJournal(),
+                ContextManager = new BasicContextManager(this.SiteUrl, this.UserName, this.Password)
+            };
+            config.MigrationProviders.Add(new ScriptMigrationProvider(this.ScriptDirectory));
+
+            var migrator = new Migrator(config);
+            migrator.PerformMigration();
+        }
+    }
+}
